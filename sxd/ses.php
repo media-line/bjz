d5fdbd89fd221b75b6803fccf1617fd5002ad55f
rakatanui <cantripsun@gmail.com>
2017-07-07 15:49:02 +0300

    ''

---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  'a6966e0df3674a0503a1d8bd52e2125b' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'root',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1499431636,
+    'lng' => 'ru',
+  ),
+);
+?>
